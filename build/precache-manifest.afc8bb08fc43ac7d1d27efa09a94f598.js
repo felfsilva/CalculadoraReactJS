2bd55edff93c1b6333f28e98b5e2c18591c02581
felfsilva <felipefsilva.00@gmail.com>
2018-12-12 11:45:35 -0200

    Projeto Calculadora

---
+++ build/precache-manifest.afc8bb08fc43ac7d1d27efa09a94f598.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b93c2ad436ccb747ae415c1e865819a4",
+    "url": "/static/media/RobotoMono-Thin.b93c2ad4.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "29132e57786ff051ce57",
+    "url": "/static/js/main.29132e57.chunk.js"
+  },
+  {
+    "revision": "5bae3a1ff5d8258f97b4",
+    "url": "/static/js/1.5bae3a1f.chunk.js"
+  },
+  {
+    "revision": "29132e57786ff051ce57",
+    "url": "/static/css/main.64a3e595.chunk.css"
+  },
+  {
+    "revision": "9211ab8855827b6d6f772ab693e92731",
+    "url": "/index.html"
+  }
+];
